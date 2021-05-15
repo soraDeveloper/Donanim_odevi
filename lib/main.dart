0e200d2527d973dac0e5be488703f187d2ea7231
suatkvam <suatkvam@gmail.com>
2021-05-15 16:42:07 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -6,7 +7,7 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
@@ -26,16 +27,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    // Icon(
-    //   Icons.check,
-    //   color: Colors.green,
-    // ),
-    // Icon(
-    //   Icons.close,
-    //   color: Colors.red,
-    // )
+  List<Icon> scoreKeeper = [];
+
+  List<Question> question = [
+    Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
+    Question(
+        q: "Approximately one quarter of human bones are in the feet.",
+        a: true),
+    Question(q: "A slug's blood is green.", a: true),
   ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                question[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -64,12 +66,20 @@ class _QuizPageState extends State<QuizPage> {
                 backgroundColor: Colors.green,
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ));
-                });
+                bool correctAnswer = question[questionNumber].questionAnswer;
+                if (correctAnswer == true) {
+                } else {}
+                setState(
+                  () {
+                    // scoreKeeper.add(
+                    //   Icon(
+                    //     Icons.check,
+                    //     color: Colors.green,
+                    //   ),
+                    // );
+                    questionNumber++;
+                  },
+                );
               },
               child: Text(
                 "True",
@@ -87,14 +97,20 @@ class _QuizPageState extends State<QuizPage> {
                 backgroundColor: Colors.red,
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
-                });
+                bool correctAnswer = question[questionNumber].questionAnswer;
+                if (correctAnswer == false) {
+                } else {}
+                setState(
+                  () {
+                    // scoreKeeper.add(
+                    //   Icon(
+                    //     Icons.close,
+                    //     color: Colors.red,
+                    //   ),
+                    // );
+                    questionNumber++;
+                  },
+                );
               },
               child: Text(
                 "False",
